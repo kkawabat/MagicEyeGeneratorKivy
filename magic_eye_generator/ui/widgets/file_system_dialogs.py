2396114fa59e8ef4487d0c91ce0db230f1ef181e
kkawabat <kakwabat@asu.edu>
2020-06-14 19:58:08 -0700

    implemented save/load file selector

--- magic_eye_generator/ui/widgets/file_system_dialogs.py
+++ magic_eye_generator/ui/widgets/file_system_dialogs.py
@@ -1,39 +1,57 @@
+from pathlib import Path
+
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
-Builder.load_file('file_system_dialogs.kv')
+
+Builder.load_file(str(Path(__file__.replace('.py', '.kv'))))
+data_dir = Path(__file__).parents[3].joinpath('data')
 
 
 class LoadDialogPopup(Popup):
-    def __init__(self, title, load_func):
-        super(LoadDialogPopup).__init__(title=title, size_hint=(0.9, 0.9),
-                                        content=LoadDialog(load=load_func, cancel=self.dismiss))
+    def __init__(self, title, load_func, default_dir=None):
+        super().__init__(title=title, size_hint=(0.9, 0.9), auto_dismiss=False,
+                         content=LoadDialog(load=load_func, cancel=self.dismiss, default_dir=default_dir))
 
 
 class LoadDialog(FloatLayout):
+    default_dir = StringProperty(str(data_dir))
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
+
+    def __init__(self, default_dir=None, **kwargs):
+        super().__init__(**kwargs)
+        self.default_dir = default_dir
 
     def run_load(self, path, selection):
         # todo sanitize input
         self.load(path, selection)
-        self.dismiss()
+        self.cancel()
+
+    @staticmethod
+    def file_filter(directory, filename):
+        return filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
 
 
 class SaveDialogPopup(Popup):
-    def __init__(self, title, save_func):
+    def __init__(self, title, save_func, default_dir):
         self.save = save_func
-        super(SaveDialogPopup).__init__(title=title, size_hint=(0.9, 0.9),
-                                        content=SaveDialog(save=save_func, cancel=self.dismiss))
+        super().__init__(title=title, size_hint=(0.9, 0.9), auto_dismiss=False,
+                         content=SaveDialog(save=save_func, cancel=self.dismiss, default_dir=default_dir))
 
 
 class SaveDialog(FloatLayout):
+    default_dir = StringProperty(str(data_dir))
     save = ObjectProperty(None)
     text_input = ObjectProperty(None)
     cancel = ObjectProperty(None)
 
+    def __init__(self, default_dir=None, **kwargs):
+        super().__init__(**kwargs)
+        self.default_dir = default_dir
+
     def run_save(self, path, text):
         # todo sanitize input
         self.save(path, text)
-        self.dismiss()
+        self.cancel()
--- magic_eye_generator/ui/widgets/magic_eye_widget.py
+++ magic_eye_generator/ui/widgets/magic_eye_widget.py
@@ -1,59 +1,76 @@
 import os
-
+from pathlib import Path
+import numpy as np
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.image import Image as CoreImage
+from kivy.uix.image import Image as KivyImage
+from PIL import Image
+from io import BytesIO
 
+from magic_eye_generator.sis_generator import gen_sis
 from magic_eye_generator.ui.widgets.file_system_dialogs import LoadDialogPopup, SaveDialogPopup
 
 Builder.load_file(r'ui\widgets\magic_eye_widget.kv')
+data_dir = Path(__file__).parents[3].joinpath('data')
 
 
 class MagicEyeWidget(BoxLayout):
-    img_source = StringProperty('texture_example_image.png')
 
     def __init__(self, **kwargs):
         super(MagicEyeWidget, self).__init__(**kwargs)
         self._popup = None
+        self.depth_val = 0
+        self.num_strips = 10
+        self.texture_fixed_width = 100
+        self.main_image_obj = KivyImage()
         self.depth_map_source = None
         self.texture_map_source = None
-        self.magic_eye_source = None
+        self.magic_eye_image = None
 
-    def load_depth_map(self):
+    def load_depth_map(self, *args):
         def load(path, filename):
             self.depth_map_source = os.path.join(path, filename[0])
+            self.view_depth_map()
+        LoadDialogPopup(title='load depth map', load_func=load, default_dir=str(data_dir.joinpath('depth_map'))).open()
 
-        LoadDialogPopup(title='load depth map', load_func=load).open()
+    def view_depth_map(self, *args):
+        self.ids.img_viewer.source = self.depth_map_source
 
-    def view_depth_map(self):
-        self.img_source = self.depth_map_source
-
-    def load_texture_map(self):
+    def load_texture_map(self, *args):
         def load(path, filename):
-            self.texture_map_path = os.path.join(path, filename[0])
+            self.texture_map_source = os.path.join(path, filename[0])
+            self.view_texture_map()
+        LoadDialogPopup(title='load texture map', load_func=load, default_dir=str(data_dir.joinpath('texture'))).open()
 
-        LoadDialogPopup(title='load texture map', load_func=load).open()
+    def view_texture_map(self, *args):
+        self.ids.img_viewer.source = self.texture_map_source
 
-    def view_texture_map(self):
-        self.img_source = self.texture_map_source
-
-    def view_magic_eye_image(self):
-        self.gen_magic_eye()
-        self.img_source = self.magic_eye_image_io.name
+    def view_magic_eye_image(self, *args):
+        self.magic_eye_image = self.gen_magic_eye()
+        self.ids.img_viewer.texture = self.magic_eye_image.texture
 
     def gen_magic_eye(self):
         # todo generate magic eye
-        pass
+        canvas_img = gen_sis(self.depth_map_source, self.texture_map_source,
+                             self.depth_val, self.num_strips, self.texture_fixed_width)
+        data = BytesIO()
+        Image.fromarray(canvas_img.astype(np.uint8), mode="RGBA").save(data, format='png')
+        data.seek(0)
+        return CoreImage(data, ext='png')
 
-    def save_magic_eye_image(self):
+    def save_magic_eye_image(self, *args):
         def save(path, filename):
-            # todo save magic_eye to file
-            os.path.join(path, filename[0])
+            self.magic_eye_image.save(os.path.join(path, filename[0]))
 
-        SaveDialogPopup(title='save magic eye image', save_func=save).open()
+        SaveDialogPopup(title='save magic eye image', save_func=save,
+                        default_dir=str(data_dir.joinpath('magic_eye_results'))).open()
 
     def update_depth(self, depth_value):
         print(f"updated depth to {depth_value}")
+        self.depth_val = depth_value
 
     def update_num_strips(self, num_strips):
         print(f"updated # strips to {num_strips}")
+        self.num_strips = num_strips
